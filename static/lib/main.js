85f59382dc0ec57b278de982542bbfbc0f237292
Yann <yann24.bonnau@hotmail.fr>
2017-06-25 18:23:28 +0200

    Complete rewrite

--- static/lib/main.js
+++ static/lib/main.js
@@ -6,12 +6,10 @@ $('document').ready(function() {
 	$(window).on('action:ajaxify.end', function(err, data) {
 		if (data.url.match(/^topic\//)) {
 			addLabel();
-			markPostAsSolved();
 		}
 	});
 
 	$(window).on('action:topic.tools.load', addHandlers);
-	$(window).on('action:post.tools.load', addPostHandlers);
 
 	$(window).on('action:posts.loaded', markPostAsSolved);
 
@@ -21,22 +19,22 @@ $('document').ready(function() {
 			return;
 		}
 
-		var item = $('<button type="button" class="btn btn-info dropdown-toggle" data-toggle="dropdown"><span class="caret"></span></button><ul class="dropdown-menu pull-right" role="menu"><li><a href="#" data-switch-action="post"><i class="fa fa-fw fa-question-circle"></i> Ask as Question</a></li></ul>');
+		var item = $('<button type="button" class="btn btn-info dropdown-toggle" data-toggle="dropdown"><span class="caret"></span></button><ul class="dropdown-menu pull-right" role="menu"><li><a href="#" data-switch-action="post"><i class="fa fa-fw fa-exclamation"></i> Mark as Important</a></li></ul>');
 		var actionBar = $('#cmp-uuid-' + data.post_uuid + ' .action-bar');
 
 		item.on('click', 'li', function() {
 			$(window).off('action:composer.topics.post').one('action:composer.topics.post', function(ev, data) {
-				callToggleQuestion(data.data.tid, false);
+				callToggleImportant(data.data.tid,false);
 			});
 		});
 
 		if (
-			config['question-and-answer'].forceQuestions === 'on' ||
-			(config['question-and-answer']['defaultCid_' + data.composerData.cid] === 'on')
+			config['important'].forceQuestions === 'on' ||
+			(config['important']['defaultCid_' + data.composerData.cid] === 'on')
 		) {
-			$('.composer-submit').attr('data-action', 'post').html('<i class="fa fa-fw fa-question-circle"></i> Ask as Question</a>');
+			$('.composer-submit').attr('data-action', 'post').html('<i class="fa fa-fw fa-exclamation"></i> Mark as Important</a>');
 			$(window).off('action:composer.topics.post').one('action:composer.topics.post', function(ev, data) {
-				callToggleQuestion(data.data.tid, false);
+				callToggleImportant(data.data.tid, false);
 			});
 		} else {
 			actionBar.append(item);
@@ -44,59 +42,28 @@ $('document').ready(function() {
 	});
 
 	function addHandlers() {
-		$('.toggleQuestionStatus').on('click', toggleQuestionStatus);
-		$('.toggleSolved').on('click', toggleSolved);
-	}
-
-	function addPostHandlers() {
-		$('[component="qanda/post-solved"]').on('click', markPostAsAnswer);
+		$('.toggleImportant').on('click', toggleImportant);
 	}
 
 	function addLabel() {
-		if (ajaxify.data.hasOwnProperty('isQuestion') && parseInt(ajaxify.data.isQuestion, 10) === 1) {
+		if (ajaxify.data.hasOwnProperty('isImportant') && parseInt(ajaxify.data.isImportant, 10) === 1) {
 			require(['components'], function(components) {
-				if (parseInt(ajaxify.data.isSolved, 10) === 0) {
-					components.get('post/header').prepend('<span class="unanswered"><i class="fa fa-question-circle"></i> Unsolved</span>');
-				} else if (parseInt(ajaxify.data.isSolved, 10) === 1) {
-					components.get('post/header').prepend('<span class="answered"><i class="fa fa-question-circle"></i> Solved</span>');
-				}
+				components.get('post/header').prepend('<span class="important"><i class="fa fa-exclamation"></i> Important</span>');
 			});
 		}
 	}
 
-	function toggleQuestionStatus() {
+	function toggleImportant() {
 		var tid = ajaxify.data.tid;
-		callToggleQuestion(tid, true);
+		callToggleImportant(tid, true);
 	}
 
-	function callToggleQuestion(tid, refresh) {
-		socket.emit('plugins.QandA.toggleQuestionStatus', {tid: tid}, function(err, data) {
-			app.alertSuccess(data.isQuestion ? 'Topic has been marked as a question' : 'Topic is now a regular thread');
+	function callToggleImportant(tid, refresh) {
+		socket.emit('plugins.Important.toggleImportantStatus', {tid: tid}, function(err, data) {
+			app.alertSuccess(data.isImportant ? 'Topic has been marked as important' : 'Topic is now a regular thread');
 			if (refresh) {
 				ajaxify.refresh();
 			}
 		});
-	}
-
-	function toggleSolved() {
-		var tid = ajaxify.data.tid;
-		socket.emit('plugins.QandA.toggleSolved', {tid: tid}, function(err, data) {
-			app.alertSuccess(data.isSolved ? 'Topic has been marked as solved' : 'Topic has been marked as unsolved');
-			ajaxify.refresh();
-		});
-	}
-
-	function markPostAsAnswer() {
-		var tid = ajaxify.data.tid;
-		var pid = $(this).parents('[data-pid]').attr('data-pid');
-
-		socket.emit('plugins.QandA.toggleSolved', {tid: tid, pid: pid}, function(err, data) {
-			app.alertSuccess(data.isSolved ? 'This post has been marked as the correct answer' : 'Topic has been marked as unsolved');
-			ajaxify.refresh();
-		});
-	}
-
-	function markPostAsSolved(err, data) {
-		$('[component="post"][data-pid="' + ajaxify.data.solvedPid + '"]').addClass('isSolved');
 	}
 });
