d8e257f03c2a1990001be5a3039848f52c68a1c0
Yann <yann24.bonnau@hotmail.fr>
2017-06-25 18:49:42 +0200

    Forget to remove stuff

--- static/lib/main.js
+++ static/lib/main.js
@@ -11,8 +11,6 @@ $('document').ready(function() {
 
 	$(window).on('action:topic.tools.load', addHandlers);
 
-	$(window).on('action:posts.loaded', markPostAsSolved);
-
 	$(window).on('action:composer.loaded', function(err, data) {
 		if (data.hasOwnProperty('composerData') && !data.composerData.isMain) {
 			// Do nothing, as this is a reply, not a new post
